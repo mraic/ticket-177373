d5539f10f128fcc8ec881623be84bd3e16fc02a4
mraic <marko.masline@gmail.com>
2024-01-22 11:32:19 +0100

    Test

--- src/main/java/com/citi/test/examples/AsposeHtmlTestApplication.java
+++ src/main/java/com/citi/test/examples/AsposeHtmlTestApplication.java
@@ -28,60 +28,61 @@ public class AsposeHtmlTestApplication {
 
         SpringApplication.run(AsposeHtmlTestApplication.class, args);
 
-        String finalPDFDir = "N/A";
-        HTMLDocument document = new HTMLDocument(htmlWithCssfileDir);
+        try {
+            String finalPDFDir = "N/A";
+            HTMLDocument document = new HTMLDocument(htmlWithCssfileDir);
 
-        PdfSaveOptions options = new PdfSaveOptions();
-        options.getPageSetup()
-                .setAnyPage(new Page(new com.aspose.html.drawing.Size(com.aspose.html.drawing.Length.fromInches(11.69),
-                        com.aspose.html.drawing.Length.fromInches(8.26)), new Margin(0, 0, 0, 0)));
-        options.getPageSetup().setAdjustToWidestPage(true);
+            PdfSaveOptions options = new PdfSaveOptions();
+            options.getPageSetup()
+                    .setAnyPage(new Page(new com.aspose.html.drawing.Size(com.aspose.html.drawing.Length.fromInches(11.69),
+                            com.aspose.html.drawing.Length.fromInches(8.26)), new Margin(0, 0, 0, 0)));
+            options.getPageSetup().setAdjustToWidestPage(true);
 
-        try (MemoryStreamProvider streamProvider = new MemoryStreamProvider()) {
-            com.aspose.html.converters.Converter.convertHTML(document, options, streamProvider.lStream);
-            java.io.InputStream memory = streamProvider.lStream.get(0);
-            memory.reset();
-            byte biyes[] = memory.readAllBytes();
-            finalPDFDir = outputPDF
-                    + new SimpleDateFormat("dd-MMM-yyyy_HH-mm-ss").format(new Date()) + ".pdf";
-            try (FileOutputStream fos = new FileOutputStream(finalPDFDir)) {
-                fos.write(biyes);
+            try (MemoryStreamProvider streamProvider = new MemoryStreamProvider()) {
+                com.aspose.html.converters.Converter.convertHTML(document, options, streamProvider.lStream);
+                java.io.InputStream memory = streamProvider.lStream.get(0);
+                memory.reset();
+                byte biyes[] = memory.readAllBytes();
+                finalPDFDir = outputPDF
+                        + new SimpleDateFormat("dd-MMM-yyyy_HH-mm-ss").format(new Date()) + ".pdf";
+                try (FileOutputStream fos = new FileOutputStream(finalPDFDir)) {
+                    fos.write(biyes);
+                }
             }
+
+
+            InputStream in = new FileInputStream(htmlfileDir);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            String line = reader.lines().collect(Collectors.joining());
+            Document doc = Jsoup.parse(line);
+            Path path = Paths
+                    .get(cssFileDir);
+            document = new HTMLDocument(doc.html(), path.toString()); // doc.html(), path.getParent().toString()
+
+            options = new PdfSaveOptions();
+            options.getPageSetup()
+                    .setAnyPage(new Page(new com.aspose.html.drawing.Size(com.aspose.html.drawing.Length.fromInches(11.69),
+                            com.aspose.html.drawing.Length.fromInches(8.26)), new Margin(0, 0, 0, 0)));
+            options.getPageSetup().setAdjustToWidestPage(true);
+
+            System.out.println("Done");
+
+
+            try (MemoryStreamProvider streamProvider = new MemoryStreamProvider()) {
+                com.aspose.html.converters.Converter.convertHTML(document, options, streamProvider.lStream);
+                java.io.InputStream memory = streamProvider.lStream.get(0);
+                memory.reset();
+                byte biyes[] = memory.readAllBytes();
+                finalPDFDir = outputPDF
+                        + new SimpleDateFormat("dd-MMM-yyyy_HH-mm-ss").format(new Date()) + ".pdf";
+                try (FileOutputStream fos = new FileOutputStream(finalPDFDir)) {
+                    fos.write(biyes);
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
 
-
-        InputStream in = new FileInputStream(htmlfileDir);
-        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-        String line = reader.lines().collect(Collectors.joining());
-        Document doc = Jsoup.parse(line);
-        Path path = Paths
-                .get(cssFileDir);
-        document = new HTMLDocument(doc.html(), path.toString()); // doc.html(), path.getParent().toString()
-        com.aspose.html.dom.Element link = document.createElement("link");
-        link.setAttribute("rel", "stylesheet");
-        link.setAttribute("href", path.toUri().toString());
-        com.aspose.html.dom.Element head = document.getElementsByTagName("head").get_Item(0);
-        head.appendChild(link);
-        options = new PdfSaveOptions();
-        options.getPageSetup()
-                .setAnyPage(new Page(new com.aspose.html.drawing.Size(com.aspose.html.drawing.Length.fromInches(11.69),
-                        com.aspose.html.drawing.Length.fromInches(8.26)), new Margin(0, 0, 0, 0)));
-        options.getPageSetup().setAdjustToWidestPage(true);
-
-        System.out.println("Done");
-
-
-        try (MemoryStreamProvider streamProvider = new MemoryStreamProvider()) {
-            com.aspose.html.converters.Converter.convertHTML(document, options, streamProvider.lStream);
-            java.io.InputStream memory = streamProvider.lStream.get(0);
-            memory.reset();
-            byte biyes[] = memory.readAllBytes();
-            finalPDFDir = outputPDF
-                    + new SimpleDateFormat("dd-MMM-yyyy_HH-mm-ss").format(new Date()) + ".pdf";
-            try (FileOutputStream fos = new FileOutputStream(finalPDFDir)) {
-                fos.write(biyes);
-            }
-        }
 
     }
 }
